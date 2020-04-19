0da755db0c36e9fc6d5b46f8a84a605705a22401
max6811 <maxc.m6811@gmail.com>
2020-04-19 08:56:21 -0400

    first platziConf App android

---
+++ app/src/main/java/com/maxidevastronaut/conf/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,13 @@
+package com.maxidevastronaut.conf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maxidevastronaut.conf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
