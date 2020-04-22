7b8cfb75589c15b1ec77a70d3d22bbfaaf417b63
max6811 <maxc.m6811@gmail.com>
2020-04-21 23:29:31 -0400

    navegacion implementada con android JetPack

--- app/src/main/java/com/maxidevastronaut/conf/ui/fragments/SpeakersDetailDialogFragment.kt
+++ app/src/main/java/com/maxidevastronaut/conf/ui/fragments/SpeakersDetailDialogFragment.kt
@@ -11,14 +11,14 @@ import com.maxidevastronaut.conf.R
 /**
  * A simple [Fragment] subclass.
  */
-class SpekersDetailDialogFragment : Fragment() {
+class SpeakersDetailDialogFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spekers_detail_dialog, container, false)
+        return inflater.inflate(R.layout.fragment_speakers_detail_dialog, container, false)
     }
 
 }
